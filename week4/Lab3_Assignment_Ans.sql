e66b3dc32a0b2c13151e8a66aaca552550a30537
Parkpoom Wisedsri <95712446+parkpoomdev@users.noreply.github.com>
2023-08-27 22:00:36 +0700

    Create Lab3_Assignment_Ans.sql

---
+++ week4/Lab3_Assignment_Ans.sql
@@ -0,0 +1,33 @@
+-- Q1: Join the film table with the language table to display the film title and its language.
+-- YOUR ANSWER HERE
+
+-- Q2: List all films and their categories using an inner join.
+-- YOUR ANSWER HERE
+
+-- Q3: List all customers and their respective rentals. Display customers even if they haven't rented any films.
+-- YOUR ANSWER HERE
+
+-- Q4: List all payments and their respective customers using a right join.
+-- YOUR ANSWER HERE
+
+-- Q5: List all films and their actors, including films with no actors and actors with no films (emulating a full join).
+-- YOUR ANSWER HERE
+
+-- Q6: Find pairs of actors who have worked together in the same film.
+-- YOUR ANSWER HERE
+
+-- Q7: List the unique names of actors and customers.
+-- YOUR ANSWER HERE
+
+-- Q8: Count the number of films in each category.
+-- YOUR ANSWER HERE
+
+-- Q9: Find categories that have more than 50 films.
+-- YOUR ANSWER HERE
+
+-- Q10: Find customers who have made at least one payment.
+-- YOUR ANSWER HERE
+
+-- Q11: Find films that have a rental duration shorter than any film in the 'Action' category.
+-- YOUR ANSWER HERE
+
